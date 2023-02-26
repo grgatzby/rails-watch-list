9dd9f6965e6c8f65ad4bd8b6dfb89f39f6ad77f2
Guillaume Cazals <gcazals06@gmail.com>
2023-02-26 21:04:58 +0100

    added  config.active_storage.service = :cloudinary in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
+  # config.active_storage.service = :local
   # 24/02 replaced :local with :cloudinary
   config.active_storage.service = :cloudinary
 
